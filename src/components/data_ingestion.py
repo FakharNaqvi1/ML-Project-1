8f379044992f4471779a76fe62a2b5e240aed938
FakharNaqvi1 <Fakharmustafa09@gmail.com>
2024-07-18 23:40:23 +0500

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,9 @@ from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass # use to create class variable
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass  #decorator
 class DataIngestionConfig:  #any input require given to this class
@@ -51,4 +54,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
